861e02b1d8f6898e6e7a2d2411e3a68564b4be0f
Haiyi Zhou <h94zhou@edu.uwaterloo.ca>
2019-01-01 18:41:51 -0800

    starting

---
+++ light_clock.ino
@@ -0,0 +1,15 @@
+/**
+ * Simple timer alarm that turns servo to turn on light
+ * basic functionality requires a screen (try sev seg?), remote, servo motor
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
