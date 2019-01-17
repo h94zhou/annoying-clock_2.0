9ac959c65196e0dc77d61bbe2b090c65ea905af7
Haiyi Zhou <h94zhou@edu.uwaterloo.ca>
2019-01-17 00:15:05 -0800

    New branch for irrec: set up basic state machine, simple transitions- todo: fine-tune and fix rotation error

--- light_clock.ino
+++ light_clock.ino
@@ -1,16 +1,15 @@
 #include <Servo.h>
-
 #include <IRremote.h>
 /**
  * Simple timer alarm that turns servo to turn on light
  * basic functionality requires a screen (try sev seg?), remote, servo motor
  */
 
-enum remote_cmd {
-  none,
-  number,     //entering number value
-  enter,      //submitting value (new timer number etc.)
-  cancel      //cancel current buffered value or press twice for removing timer?
+enum machine_state {
+  transition_on,
+  transition_off,
+  on,
+  off
 };
 
 const int RECV_PIN = 7;
@@ -19,7 +18,9 @@ decode_results results;
 
 Servo servo1;
 
-static remote_cmd state = none;
+static machine_state state = off;
+long command_val;
+
 static int angle = 0;
 int change;
 
@@ -37,20 +38,51 @@ void loop() {
   // put your main code here, to run repeatedly:
   //while time is > 0, cycle through timer and count down, checking for remote input
   if (irrecv.decode(&results)){
-    Serial.println(results.value, HEX);
-    switch (results.decode_type){
-      
+    command_val = results.value;
+    Serial.println(command_val, HEX);
+    switch (command_val){
+      case 0xFFA25D:
+        //power: unsure of behaviour
+        break;
+      case 0xFFE01F:
+        //down
+        if (state == on){
+          state = transition_off; 
+        }
+        break;
+      case 0xFF906F:
+        //up 
+        if (state == off){
+          state = transition_on;
+        }
+      default:
+        //no input
+        break;
     }
     irrecv.resume();
   }
-
-  if (angle >= 180){
-    change = -1;
-  }else if (angle <= 0){
-    change = 1;
+  switch (state){
+    case transition_on:
+      if (angle < 180){
+        servo1.write(angle += 1);
+        delay(15);
+      }else {
+        state = on;
+      }
+      break;
+    case transition_off:
+      if (angle > 0){
+        servo1.write(angle -= 1);
+        delay(15);
+      }else {
+        state = off;
+      }
+      break;
+    case on:
+    case off:
+    default:
+      break;
   }
-  servo1.write(angle += change);
-  delay(15);
 }
 
 
